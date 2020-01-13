19181cf07ab5491a8e6f8754016dc031a8c381da
fanyingnan <fanyingnan@meituan.com>
2020-01-13 09:07:44 +0800

    新增解题思路

--- minCountCoins.js
+++ minCountCoins.js
@@ -1,12 +1,45 @@
-// 贪心
-function minCountCoins(coins, amount) {
-  // //
+// BF
+function minCountCoinsBF(coins, amount) {
+  function subMinCounts(coins, coinIndex, amount) {
+    var minCount = Infinity;
+    for(var i = 0; i <= parseInt(amount / coins[coinIndex]); i++) {
+      if(amount-coins[coinIndex]*i === 0) {
+        if(i < minCount) minCount = i;
+      }else {
+        if(coinIndex === coins.length-1) continue; // exit-point
 
-  // return checksCount;
-};
+        var subCount = subMinCounts(coins, coinIndex+1, amount-coins[coinIndex]*i);
+        if(subCount + i < minCount) minCount = subCount + i;
+      }
+    }
+    return minCount;
+  }
+  var minCount = subMinCounts(coins, 0, amount);
+  return minCount === Infinity ? -1 : minCount; 
+ };
+
+// Greedy
+// function minCountCoinsGreedy(coins, amount) {
+// };
+
+// merge
+function minCountCoinsMerge(coins, amount) {
+  function subMinCountCoins(coins, amount) {
+    var minCount = Infinity;
+    for(var i=1; i<=coins.length; i++) {
+      if(coins[i]>amount) continue; // exit-point
+
+      var temp = subMinCountCoins(coins,amount-coins[i]);
+      if(temp+1 < minCount) minCount = temp + 1;
+    }
+  }
+
+  var minCount = subMinCountCoins(coins, amount);
+  return minCount===Infinity ? Infinity : minCount;
+}
 
 // DP
-function minCounCoinsDP(coins, amount) {
+function minCountCoinsDP(coins, amount) {
   let solutions = [];
 
   let sub = -1;
@@ -40,7 +73,23 @@ function minCounCoinsDP(coins, amount) {
   return solutions[amount];
 }
 
+// DP-opt
+function minCountCoinsDPopt(coins, amount) {
+  let minCounts = [];
+  minCounts[0] = 0;
+  for(var i=1; i<=amount; i++) {
+    let minCount = Infinity;
+    for(var j=0; j<coins.length; j++) {
+      if(coins[j]>i) continue; // cut-subTree
+      if(minCounts[i-coins[j]]+1 < minCount) {
+        minCount = minCounts[i-coins[j]]+1;
+      }
+    }
+    minCounts[i] = minCount;
+  }
+  return minCounts[amount] === Infinity ? -1 : minCounts[amount];
+}
 
 let coins = [1,2,5];
 let P = 11;
-console.log(minCounCoinsDP(coins, P));
+console.log(minCountCoinsDPopt(coins, P));
